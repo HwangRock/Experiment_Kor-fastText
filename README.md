76ce9983cb762738be5b3e47b0e00c65aa17375e
HwangRock <157935545+HwangRock@users.noreply.github.com>
2025-07-04 19:48:05 +0900

    feat: 모델 평가 코드 - matric은 평균 코사인 유사도

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Experiment_Kor-fastText  
+## Experiment_Kor-fastText  
 
 한국어 fastText를 fine tuning 시켜서 유사도를 높이거나, 경량화를 시켜보는 레포지토리입니다.  
 아래의 링크에서 한국어 fastText를 받을 수 있습니다.  
@@ -6,14 +6,14 @@ https://fasttext.cc/docs/en/crawl-vectors.html
 
 ---
 
-# 시작 계기
+## 시작 계기
 https://github.com/HwangRock/notion-serverless-voca  
 위의 프로젝트를 하다가 워드 임베딩을 통한 유사도를 기반으로 한 영단어 채점으로 서비스의 질을 높여보기로 했습니다.  
 단어 간 유사도만을 계산하면 되므로 Bert보다는 더 가벼운 fastText를 사용하기로 했습니다.  
 
 ---
 
-# pre-trained Kor-fastText 실험 후기
+## pre-trained Kor-fastText 실험 후기
 ./experiment_PreTrain/main.py의 코드로 실험을 했습니다.  
 Bert보다 가볍다고 해서 KB단위를 기대했는데 7.2GB로 생각보다 무거웠습니다.  
 그러다보니 서버에 올리는 것만해도 약 8이 걸려서 FaaS로 사용하려는 의도는 포기했습니다.  
@@ -26,7 +26,7 @@ pre-train된 모델이라 그런지 유사도 계산도 그렇게 정확하지�
 ###### 반의어를 했을 때  
 ![](presentation/ex2.png)  
 
-# How to improve?
+## How to improve?
 2가지로 실험을 해볼 생각입니다.  
 FaaS로 쓸 수 있을 정도는 아니더라도 경량화를 시켜서 성능이 비슷하고 나오게 하기.  
 fine-tuning을 시켜서 더 높은 유사도를 나오게 하기.  
---
+++ scripts/eval.py
@@ -0,0 +1,56 @@
+from utils.dataPreprocess import text_preprocess
+from gensim import models
+import time
+
+
+def main():
+    eval_synonym_data = "data/eval_synonym_data.txt"
+    eval_antonym_data = "data/eval_antonym_data.txt"
+
+    data_pairs = text_preprocess(eval_synonym_data)
+
+    print("모델을 서버에 업로드 중입니다...")
+    start_time = time.time()
+
+    ko_model = models.fasttext.load_facebook_model('cc.ko.300.bin')
+
+    end_time = time.time()
+    elapsed = end_time - start_time
+
+    print("모델 업로드 완료!")
+    print(f"업로드에 소요된 시간: {elapsed:.3f}초")
+    print("=" * 20)
+    data_len = len(data_pairs)
+    print("총 " + str(data_len) + "개의 데이터로 유의어 평가를 진행합니다.")
+
+    synonym_score = 0
+    antonym_score = 0
+
+    score = 0
+    for i, j in enumerate(data_pairs):
+        similarity = ko_model.wv.similarity(j[0], j[1])
+        print(f"{i}: {j[0]}과 {j[1]}의 코사인 유사도: {similarity:.4f}")
+        score += similarity
+    synonym_score = score / data_len
+    data_pairs.clear()
+    print("=" * 20)
+    print("유의어 테스트 종료. 반의어 테스트 시작")
+
+    data_pairs = text_preprocess(eval_antonym_data)
+    data_len = len(data_pairs)
+    print("총 " + str(data_len) + "개의 데이터로 반의어 평가를 진행합니다.")
+
+    score = 0
+    for i, j in enumerate(data_pairs):
+        similarity = ko_model.wv.similarity(j[0], j[1])
+        print(f"{i}: {j[0]}과 {j[1]}의 코사인 유사도: {similarity:.4f}")
+        score += similarity
+    antonym_score = score / data_len
+
+    print("=" * 20)
+    print(f"유의어의 평균 유사도 : {synonym_score:.4f}")
+    print(f"반의어의 평균 유사도 : {antonym_score:.4f}")
+
+
+if __name__ == "__main__":
+    main()
